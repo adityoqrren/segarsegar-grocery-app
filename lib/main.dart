7d73a44c8796d1e308afb5dc8e48572d2f53eb8f
adityoqrren <aji.adityo8@gmail.com>
2023-11-21 16:41:37 +0700

    little changes

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Segar segar',
+      title: 'segarSegar',
       theme: ThemeData(
         // This is the theme of your application.
         primarySwatch: Colors.blue,
